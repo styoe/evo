e9d0c63b816ac863cd70ad5daf416aa320ebdf01
Luka Maljic <luka.maljic@gmail.com>
2018-02-18 23:16:50 +0100

    Simple growth animation & git cache cleanup

--- Assets/Main/PlayerCollision.cs
+++ Assets/Main/PlayerCollision.cs
@@ -2,28 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCollision : MonoBehaviour {
+public class PlayerCollision : MonoBehaviour
+{
 
     private int cookiesEaten;
     private SphereCollider sphereColider;
 
-	void Start () {
+    void Start()
+    {
         sphereColider = GetComponent<SphereCollider>();
-		cookiesEaten = 0;
-	}
-	
-    void OnCollisionEnter (Collision col) {
-        if(col.gameObject.name == "Cookie") {
+        cookiesEaten = 0;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Cookie")
+        {
             //Make it grow
             var playerScale = this.gameObject.transform.localScale.x;
             var cookieScale = col.gameObject.transform.localScale.x;
 
-            if(playerScale > cookieScale){
+            if (playerScale > cookieScale)
+            {
                 // Calculate the scale and radius by adding volumes of cookie and player
                 float playerVolume = (4f / 3f) * Mathf.PI * Mathf.Pow(playerScale, 3);
                 float cookieVolume = (4f / 3f) * Mathf.PI * Mathf.Pow(cookieScale, 3);
-                float newPlayerVolume = playerVolume + cookieVolume; 
-                float newPlayerRadius =  Mathf.Pow(newPlayerVolume / (4f/3f * Mathf.PI), 1f/3f);
+                float newPlayerVolume = playerVolume + cookieVolume;
+                float newPlayerRadius = Mathf.Pow(newPlayerVolume / (4f / 3f * Mathf.PI), 1f / 3f);
                 float newPlayerScale = newPlayerRadius * 2;
                 float scaleIncrement = newPlayerScale - playerScale;
 
@@ -31,27 +36,49 @@ public class PlayerCollision : MonoBehaviour {
                 Debug.Log(newPlayerScale);
 
                 // Set scale and radius of player
-                this.gameObject.transform.localScale = new Vector3(scaleIncrement, scaleIncrement, scaleIncrement);
+                // this.gameObject.transform.localScale = new Vector3(scaleIncrement, scaleIncrement, scaleIncrement);
                 // sphereColider.radius = newPlayerRadius;
 
+                StartCoroutine(Grow(new Vector3(scaleIncrement, scaleIncrement, scaleIncrement)));
+
                 //destroy cookie
-                Destroy (col.gameObject);
+                Destroy(col.gameObject);
 
                 cookiesEaten += 1;
-            }            
+            }
         }
 
-        if(col.gameObject.tag == "Powerup") {
+        if (col.gameObject.tag == "Powerup")
+        {
             //Add script
             addScript(this.gameObject, col.gameObject.name);
             Destroy(col.gameObject);
         }
     }
 
-    void addScript(GameObject obj, string ScriptName) {
-        System.Type ScriptType = System.Type.GetType (ScriptName + ",Assembly-CSharp");
-        if(!obj.GetComponent (ScriptName)) {  
-            obj.AddComponent (ScriptType);
+
+    IEnumerator Grow(Vector3 targetScale)
+    {
+        float duration = 1;
+        float elapsedTime = 0;
+
+        while (elapsedTime <= duration)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, targetScale, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        transform.localScale = targetScale;
+    }
+
+    // ne moras programativno dodavati skripte nego samo obj.AddComponent<T>(); di je T type definiran u skripti
+    void addScript(GameObject obj, string ScriptName)
+    {
+        System.Type ScriptType = System.Type.GetType(ScriptName + ",Assembly-CSharp");
+        if (!obj.GetComponent(ScriptName))
+        {
+            obj.AddComponent(ScriptType);
         }
     }
 }
